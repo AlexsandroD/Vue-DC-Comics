feb8d6f5ae71932439949b1749c137854ed073af
AlexsandroD <adrdra88@gmail.com>
2021-12-13 16:29:25 +0100

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,77 @@
+<template>
+  <header>
+      <div class="container">
+      <div class="logo"><img src="../assets/img/dc-logo.png" alt="Logo Dc"></div>
+      <div class="nav">
+          <ul>
+              <li><a href="">characters</a></li>
+              <li><a href="">comics</a></li>
+              <li><a href="">movies</a></li>
+              <li><a href="">tv</a></li>
+              <li class="active"><a href="">games</a></li>
+              <li><a href="">collectibles</a></li>
+              <li><a href="">videos</a></li>
+              <li><a href="">fans</a></li>
+              <li><a href="">news</a></li>
+              <li><a href="">shop</a></li>
+          </ul>
+      </div>
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+name:"Header"
+}
+</script>
+
+<style scoped lang="scss">
+    header{
+        height: 100px;
+
+       
+     .logo{
+            display: flex;
+            align-items: center;
+            justify-content: start;
+            width: 20%;
+            height: 100%;
+            margin-right: 100px;
+            img{
+                width: 60px;
+            }
+        }
+     .nav{
+         display: flex;
+         justify-content: center;
+         align-items: center;
+         height: 100%;
+         width: 80%;
+         ul{
+             display: flex;
+             height: 100%;
+             align-items: center;
+             li{
+                 display: flex;
+                 align-items: center;
+                 height: 100%;
+                 list-style: none;
+                 padding: 0px 10px;
+                 text-transform: uppercase;
+
+                 &:hover {
+                     border-bottom: 5px solid blue;
+                 }
+                 
+                 a{
+                     text-decoration: none;
+                     color: black;
+                     font-weight: 600;
+                 }
+               
+             }
+         }
+     }
+    }
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+      <div class="content">--> Content goes here</div>
+  </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="scss">
+main{
+    height: 200px;
+    background-color: black;
+    .content{
+        color: white;
+        font-size: 40px;
+        line-height: 200px;
+    }
+}
+</style>
